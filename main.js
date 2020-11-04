e2e491495fd78afd7a68a9af8ff47f0418268558
Tamihere <toka@tamihere.com>
2020-11-04 20:10:17 +1300

    added html, css and js

--- main.js
+++ main.js
@@ -1,1 +1,61 @@
 // JavaScript Document
+//Make the ID element toggle visibility:
+function toggle_visibility(id) 
+{
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+}
+
+//Make the DIV element draggagle:
+
+var draggableElements = document.getElementsByClassName("draggable");
+
+for(var i = 0; i < draggableElements.length; i++){
+    dragElement(draggableElements[i]);
+}
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "")) {
+        document.getElementById(elmnt.id + "").onmousedown = dragMouseDown;
+    } else {
+        elmnt.onmousedown = dragMouseDown;
+    }
+    function dragMouseDown(e) {
+        e = e || window.event;
+        pos3 = parseInt(e.clientX);
+        pos4 = parseInt(e.clientY);
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+        return false;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        pos1 = pos3 - parseInt(e.clientX);
+        pos2 = pos4 - parseInt(e.clientY);
+        pos3 = parseInt(e.clientX);
+        pos4 = parseInt(e.clientY);
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        console.log(elmnt.offsetTop)
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
+var autoPlayVideo =
+	document.getElementById(id);
+		autoPlayVideo.oncanplauthrough =
+function() {
+			autoPlayVideo.muted = true;
+			autoPlayVideo.play();
+			autoPlayVideo.pause();
+			autoPlayVideo.play();
+		}
